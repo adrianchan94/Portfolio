ad4bde35ea3b4cbe115e310eb6511dc1e392cd83
Adrian Chan <adrianchanuk@gmail.com>
2021-02-23 22:38:09 +0800

    Footer Completed

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Experience = () => {
     return (
-        <div className="experience">
+        <div id="experience" className="experience">
             <div className="d-flex justify-content-center my-5">
             <h1>Experience</h1>
             </div>
@@ -10,48 +10,51 @@ const Experience = () => {
                 <div className="timeline-block timeline-block-right">
                     <div className="marker"></div>
                     <div className="timeline-content">
-                        <h3>2014-15</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
-                            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
-                            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
-                            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                        <h3>2014-16</h3>
+                        <p>During my time in university, I co-founded and ran an English online entertainment magazine called MadbuzzHK with two friends.
+                        As an editor of the magazine, I developed, edited and moderated weekly articles for the website. 
+                        Our magazine was selected as one of twelve finalists for the 2014 StartmeupHK Venture Programme (Out of 550 applicants and 47 countries).
+                        </p>
                     </div>
                 </div>
                 {/* - */}
                 <div className="timeline-block timeline-block-left">
                     <div className="marker"></div>
                     <div className="timeline-content">
-                        <h3>2016-17</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
-                            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
-                            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
-                            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                        <h3>2017-2018</h3>
+                        <p>After university, I worked as an marketing executive for London-based Blockchain-VR start-up, ImmVRse. I assisted in the development of their platform concept and company whitepaper, 
+                            and subsequently helped execute their marketing and PR campaign leading up to their first ever Initial-Coin Offering(ICO). 
+                            <br/><br />
+                            My role was wide-ranging and consisted of producing and moderating weekly press releases and blog posts, managing and running advertising campaigns across all their social media accounts. 
+                            I managed a community of 25K members on Telegram, liasing with potential partners, private investors and contributors, and also assisted in their exhibition at the Blockchain Summit 2018 and
+                            a 3-week research project with Imperial College London on the impact of VR on neural activity.</p>
                     </div>
                 </div>
                 {/* - */}
                 <div className="timeline-block timeline-block-right">
                     <div className="marker"></div>
                     <div className="timeline-content">
-                        <h3>2017-18</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
-                            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
-                            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
-                            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                        <h3>2019-2020</h3>
+                        <p>In 2019 I joined Elate Communications, a London-based PR/Communications agency. As an account executive, my primary role was to developed content including press releases,
+                            news stories, articles, presentations, case studies and product pieces on behalf of clients, and subsequently pitching them to national, regional and trade journalists, across 
+                            print, broadcast and online. 
+                            <br /><br />
+                            During my time with Elate, I also facilitated many interviews and editorial meetings with key journalists for clients spokespeople, in addition to handling clients' social
+                            media accounts on Twitter, Facebook and LinkedIn, delivering analytics reports on traffic, engagement and follower figures.
+                         </p>
                     </div>
                 </div>
                  {/* - */}
                  <div className="timeline-block timeline-block-left">
                     <div className="marker"></div>
                     <div className="timeline-content">
-                        <h3>2018-20</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
-                            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
-                            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
-                            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                        <h3>2020-Present</h3>
+                        <p>Halfway through 2020, I decided to move back to Hong Kong and made the decision to fulfil a long-time goal of mine and pursue a new career as a developer. 
+                        <br /><br />
+                        I completed Xccelerate's Full-Stack Software Development Bootcamp with no prior IT background and learnt technologies 
+                        including JavaScript, HTML, CSS, jQuery, Bootstrap, Node.js, Express.js, PostgreSQL, Socket.io and React, in addition to completing 
+                        several portfolio projects. 
+                        </p>
                     </div>
                 </div>
             </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import logo from '../Images/logo.png'
+import { Link } from "react-scroll";
 //React Font Awesome Imports
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from "@fortawesome/free-solid-svg-icons"
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark fixed-top">
 
       <div className="container">
         <div className="container-fluid">
@@ -18,16 +19,19 @@ const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link" href="/#">Home</a>
+                <Link smooth={true} to="home" offset={0} className="nav-link" href="/#">Home</Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/#">Experience</a>
+                <Link smooth={true} to="about" offset={-60} className="nav-link" href="/#">About Me</Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/#">Projects</a>
+                <Link smooth={true} to="experience" offset={-100} className="nav-link" href="/#">Experience</Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/#">Contact</a>
+                <Link smooth={true} to="projects" offset={0} className="nav-link" href="/#">Projects</Link>
+              </li>
+              <li className="nav-item">
+                <Link smooth={true} to="contacts" offset={0} className="nav-link" href="/#">Contact Me</Link>
               </li>
             </ul>
           </div>
