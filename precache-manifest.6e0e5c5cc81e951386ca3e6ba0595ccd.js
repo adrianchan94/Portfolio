d0a97f8e62698fc41e377cfbf20942a5385543d5
Adrian Chan <adrianchanuk@gmail.com>
2021-02-27 12:23:17 +0800

    Updates

---
+++ precache-manifest.6e0e5c5cc81e951386ca3e6ba0595ccd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7423c49d7513a469e0",
+    "url": "/Portfolio/static/css/main.c1f5e205.chunk.css"
+  },
+  {
+    "revision": "ee7423c49d7513a469e0",
+    "url": "/Portfolio/static/js/main.ee7423c4.chunk.js"
+  },
+  {
+    "revision": "7ac5d3a9fa71d240c1d5",
+    "url": "/Portfolio/static/js/1.7ac5d3a9.chunk.js"
+  },
+  {
+    "revision": "cc5653277afd96f32a97",
+    "url": "/Portfolio/static/css/2.9e23c08b.chunk.css"
+  },
+  {
+    "revision": "cc5653277afd96f32a97",
+    "url": "/Portfolio/static/js/2.cc565327.chunk.js"
+  },
+  {
+    "revision": "ab34c6811dd6dbcb21f3",
+    "url": "/Portfolio/static/js/runtime~main.ab34c681.js"
+  },
+  {
+    "revision": "e53c4bf03caa0289a49ba6a85e180434",
+    "url": "/Portfolio/static/media/OnlyBrands.e53c4bf0.png"
+  },
+  {
+    "revision": "2921c7b09756ff9bb0547225a2a31a71",
+    "url": "/Portfolio/static/media/MunchM8.2921c7b0.png"
+  },
+  {
+    "revision": "7a47dad933f79836e2d64b67a30f37f9",
+    "url": "/Portfolio/static/media/CanvasBoys.7a47dad9.png"
+  },
+  {
+    "revision": "09f9c7fe69978da7dce67eb6523e4fd8",
+    "url": "/Portfolio/static/media/logo.09f9c7fe.png"
+  },
+  {
+    "revision": "65e68f21564b19a6c33b8246fcbb6881",
+    "url": "/Portfolio/static/media/adrian.65e68f21.jpg"
+  },
+  {
+    "revision": "76a2bea89442c692113732a743320537",
+    "url": "/Portfolio/static/media/hongkong.76a2bea8.jpg"
+  },
+  {
+    "revision": "26babf0e25b8255c468fd58ae8b9101f",
+    "url": "/Portfolio/index.html"
+  }
+];
